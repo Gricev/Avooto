3764972da88dca472dff3b752ccda7edd235dea4
Your Name <gricev20@mail.ru>
2022-06-22 17:25:33 +0300

    new functions were added to this project.

--- src/main/java/com/example/Avooto/controllers/ProductController.java
+++ src/main/java/com/example/Avooto/controllers/ProductController.java
@@ -13,11 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.security.Principal;
-import java.util.ArrayList;
-import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
--- src/main/java/com/example/Avooto/servicies/ProductService.java
+++ src/main/java/com/example/Avooto/servicies/ProductService.java
@@ -13,10 +13,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.IOException;
 import java.security.Principal;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
 
 @Service
 @RequiredArgsConstructor
